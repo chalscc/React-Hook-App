7937a9acdbbfa967e6d28053a13d4f6165c22ace
chalscc <carlescatalancalabuig@gmail.com>
2023-12-02 16:56:19 +0100

    Atacando a API, ya nos muestra el producto

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -6,14 +6,34 @@ export const MultipleCustomHooks = () => {
     const url = 'https://fakestoreapi.com/products/1'
     const { data, isLoading, hasError } = useFetch(url)
 
-    console.log(data, isLoading, hasError)
+    console.log(data)
+
+    const { title, description } = !!data && data
 
     return (
         <>    
-            <h1>BreakingBad Quotes</h1>
+            <h1>Mis productos</h1>
             <hr/>
+
+            {
+            isLoading 
+                ? (
+                    <div className='alert alert-info text-center'>
+                        Loading...
+                    </div>
+                ) : (                    
+                    <>
+                        <blockquote className='blockquote text-end'>
+                            <p className='mb-3'>{title}</p>
+                            <footer className='blockquote-footer'> {description} </footer>
+                        </blockquote>
+                        <button className='btn btn-primary'>
+                            Siguiente producto
+                        </button>
+                    </>
+                )
+            }            
+
         </>
-        
-        
     )
 }
