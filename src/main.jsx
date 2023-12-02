523641898f2c3359abe4525c1f2724c3598322f5
chalscc <carlescatalancalabuig@gmail.com>
2023-12-02 17:31:36 +0100

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx'
 import { SimpleForm } from './02-useEffect/SimpleForm.jsx'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks.jsx'
+import { FocusScreen } from './04-useRef/FocusScreen.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <CounterWithCustomHook /> */}
     {/* <SimpleForm /> */}
     {/* <FormWithCustomHook /> */}
-    <MultipleCustomHooks />
+    {/* <MultipleCustomHooks /> */}
+    <FocusScreen />
   </React.StrictMode>,
 )
