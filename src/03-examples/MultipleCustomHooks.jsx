6a66923edfd35c69092655bbd99b9cf781835163
chalscc <carlescatalancalabuig@gmail.com>
2023-12-02 16:40:24 +0100

    custom hook useFetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,14 +4,16 @@ import { useFetch } from '../hooks/useFetch'
 export const MultipleCustomHooks = () => {
 
     const url = 'https://fakestoreapi.com/products/1'
-    const {  } = useFetch(url)
+    const { data, isLoading, hasError } = useFetch(url)
 
-  return (
-    <>    
-        <h1>BreakingBad Quotes</h1>
-        <hr/>
-    </>
-    
-    
-  )
+    console.log(data, isLoading, hasError)
+
+    return (
+        <>    
+            <h1>BreakingBad Quotes</h1>
+            <hr/>
+        </>
+        
+        
+    )
 }
