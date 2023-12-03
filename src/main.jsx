a76b649b319fa0629dc200341c54ba20118bb7fd
chalscc <carlescatalancalabuig@gmail.com>
2023-12-03 17:12:12 +0100

    Ejemplo de Reducer sencillo

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import { Memorize } from './06-memos/Memorize.jsx'
 import { MemoHook } from './06-memos/MemoHook.jsx'
 import { CallbackHook } from './06-memos/CallbackHook.jsx'
 import { Padre } from './07-tarea-memo/Padre.jsx'
+import './08-useReducer/intro-reducer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -27,6 +28,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Memorize /> */}
     {/* <MemoHook /> */}
     {/* <CallbackHook /> */}
-    <Padre />
+    {/* <Padre /> */}
+
+    
   </React.StrictMode>,
 )
