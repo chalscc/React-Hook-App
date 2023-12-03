d3014713be0568aa3204cf043cbcfa1532300131
chalscc <carlescatalancalabuig@gmail.com>
2023-12-03 13:13:19 +0100

    Demostración funcional de hooks de memorización

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import { Layout } from './05-useLayoutEffect/Layout.jsx'
 import { Memorize } from './06-memos/Memorize.jsx'
 import { MemoHook } from './06-memos/MemoHook.jsx'
 import { CallbackHook } from './06-memos/CallbackHook.jsx'
+import { Padre } from './07-tarea-memo/Padre.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Layout /> */}
     {/* <Memorize /> */}
     {/* <MemoHook /> */}
-    <CallbackHook />
+    {/* <CallbackHook /> */}
+    <Padre />
   </React.StrictMode>,
 )
