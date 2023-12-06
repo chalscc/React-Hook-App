733c62639f3c5119ba84202c699b91a5d5e2e9ec
chalscc <carlescatalancalabuig@gmail.com>
2023-12-06 12:15:06 +0100

    Link tags

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, Link } from 'react-router-dom'
 import { HomePage } from './HomePage'
 import { AboutPage } from './AboutPage'
 import { LoginPage } from './LoginPage'
@@ -8,6 +8,12 @@ export const MainApp = () => {
   return (
     <>
       <h1> MainApp </h1>
+
+        {/* Funciona como el anchor tag de HTML <a href=""></a> per sin re-renderizar toda la pantalla */}
+        {/* Lo utilizariamos para navegar a sitios externos principalmente */}
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/about">About</Link>
       <hr/>
 
       <Routes>
