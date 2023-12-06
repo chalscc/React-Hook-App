63290c3bb664663dfccc416660132bc1ab8a5c30
chalscc <carlescatalancalabuig@gmail.com>
2023-12-06 12:11:27 +0100

    Navegacion a rutas

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { HomePage } from './HomePage'
+import { AboutPage } from './AboutPage'
+import { LoginPage } from './LoginPage'
 
 export const MainApp = () => {
   return (
     <>
-    <h1>
-      MainApp
-    </h1>
-    <br></br>
+      <h1> MainApp </h1>
+      <hr/>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+
+        {/* <Route path="/*" element={<h1>404 Not Found</h1>} /> */}
+        {/* Si introduces una ruta no existente, te redirige a about */}
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </>
   )
 }
